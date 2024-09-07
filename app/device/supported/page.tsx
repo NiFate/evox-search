e43a459b2e218d5896c2fa28a1738c985877ba30
NiFate <artik.shamin@gmail.com>
2024-09-07 15:20:45 +0300

    Added sorting for device list

--- app/device/supported/page.tsx
+++ app/device/supported/page.tsx
@@ -13,12 +13,19 @@ import { Badge } from "@/components/ui/badge";
 import { useDevices } from "@/hooks/useDevices";
 import Link from "next/link";
 import Loader from "@/components/loader";
+import { firstBy } from "thenby"
 
 export default function TestPage() {
 
   const { data, isLoading } = useDevices();
 
   if (isLoading) { return <Loader /> }
+
+  const oemCounts = data?.reduce((acc, device) => {
+    const oemKey = device.meta.oem.toLowerCase();
+    acc[oemKey] = (acc[oemKey] || 0) + 1;
+    return acc;
+  }, {});
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen px-4 py-12 sm:px-6 lg:px-8 gap-4">
@@ -34,19 +41,27 @@ export default function TestPage() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {data?.map((device, index) => (
-                <TableRow>
-                  <TableCell className="font-medium">{device.meta.oem}</TableCell>
-                  <TableCell className="font-medium">
-                    <Link href={`/device/${device.codename}`}>
-                      {device.meta.device}
-                    </Link>
-                  </TableCell>
-                  <TableCell>
-                    <Badge variant="secondary">{device.codename}</Badge>
-                  </TableCell>
-                </TableRow>
-              ))}
+              {
+                data?.sort(
+                  firstBy((a, b) => {
+                    const oemA = oemCounts[a.meta.oem.toLowerCase()] || 0;
+                    const oemB = oemCounts[b.meta.oem.toLowerCase()] || 0;
+                    return oemB - oemA;
+                  })
+                ).map((device, index) => (
+                  <TableRow key={index}>
+                    <TableCell className="font-medium">{device.meta.oem}</TableCell>
+                    <TableCell className="font-medium">
+                      <Link href={`/device/${device.codename}`}>
+                        {device.meta.device}
+                      </Link>
+                    </TableCell>
+                    <TableCell>
+                      <Badge variant="secondary">{device.codename}</Badge>
+                    </TableCell>
+                  </TableRow>
+                ))
+              }
             </TableBody>
           </Table>
         </CardContent>
